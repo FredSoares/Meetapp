c4d3a3cee77e22890dc9fa01006a56783166f9bc
Frederico Soares <soaresfred89@gmail.com>
2019-11-19 11:01:21 -0100

    Update user information

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,8 +19,37 @@ class UserController {
     });
   }
 
+  /* method to update user information */
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    /* select user with id */
+    const user = await User.findByPk(req.userId);
+
+    /* if email is different the user email, check if the email is already exists */
+    if (email !== user.email) {
+      // get user email to verify email already exists
+      const userExists = await User.findOne({ where: { email } });
+
+      // if exists return error status 401
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+    /* verify if the user password is valid if user send oldPassword */
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Password does not match' });
+    }
+
+    /* if all is correct update the user */
+    const { id, name } = await user.update(req.body);
+
+    /* return user information */
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
